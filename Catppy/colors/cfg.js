2629c6a02953c9edb0dff13c740114ec62191dc4
mki77 <mki77@users.noreply.github.com>
2026-04-01 18:28:06 +0200

    ‎

---
+++ Catppy/colors/cfg.js
@@ -0,0 +1,8 @@
+export default {
+  multipass: true, // boolean
+  plugins: [
+    'preset-default', // built-in plugins enabled by default
+    'removeOffCanvasPaths',
+    'removeRasterImages',
+  ],
+};
