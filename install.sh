fbfe8145cf671c98b5b38be5a0f22455923217e1
mki77 <mki77@users.noreply.github.com>
2026-04-04 17:24:16 +0200

    ‎

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 echo "Installing icon theme..."
 iconame=Ashy
 icodir=~/.icons
-#ln -s $icodir ~/.local/share/icons
-rsync -a $iconame -t $icodir
-rsync -a colors -t $icodir/$iconame
+ln -s $icodir ~/.local/share/icons
+rsync -a $iconame -t $icodir --del
+rsync -a colors -t $icodir/$iconame --del
 gtk-update-icon-cache -f $iconame
 exit 0
 
